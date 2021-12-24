1fb0a3fd15092656caf74b1716aab74cceb9468e
plotinusspascual <plotinusspascual@gmail.com>
2021-12-23 16:40:22 -0800

    added variables containing characters for password

--- script.js
+++ script.js
@@ -6,9 +6,22 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = password; 
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+var pwdCharLower = "0123456789abcdefghijklmnopqrstuvwxyz";
+var pwdCharUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var pwdCharSpecial = "!@#$%^&*()";
+
+var pwdCharArray = pwdCharLower.split('');
+var pwdCharArray = pwdCharUpper.split('');
+var pwdCharArray = pwdCharSpecial.split('');
+
+var generatePwd = {
+  
+}
