e3f19dcc8ab731c959c37294ce37578a31f7c396
plotinusspascual <plotinusspascual@gmail.com>
2021-12-23 17:02:50 -0800

    added required length of characters for password

--- script.js
+++ script.js
@@ -13,15 +13,23 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
+// Available Characters to use
 var pwdCharLower = "0123456789abcdefghijklmnopqrstuvwxyz";
 var pwdCharUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var pwdCharSpecial = "!@#$%^&*()";
 
+// Seperating string to make an array 
 var pwdCharLowerArr = pwdCharLower.split('');
 var pwdCharUpperArr = pwdCharUpper.split('');
 var pwdCharSpecialArr = pwdCharSpecial.split('');
 
-var generatePwd = {
+// Prompt for length of characters
+function generatePassword(){
+  var lengthOfPwd = (prompt("How many characters do you want your password to be?"));
   
+  while(lengthOfPwd < 8 && lengthOfPwd < 128){
+    alert("Password must be at least 8 characters long and at most 128 characters long");
+    break;
+  }
 }
+  
